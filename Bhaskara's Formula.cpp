d36169f0fa11f8b711247352803b2f4f404bd088
Ahmed Muhammed <75481578+mr-01000001@users.noreply.github.com>
2021-12-21 12:55:09 +0200

    Update Bhaskara's Formula.cpp
    Read 3 floating-point numbers. After, print the roots of bhaskara’s formula. If it's impossible to calculate the roots because a division by zero or a square root of a negative number, presents the message “Impossivel calcular”.
    
    Input
    Read 3 floating-point numbers (double) A, B and C.
    
    Output
    Print the result with 5 digits after the decimal point or the message if it is impossible to calculate.

--- Bhaskara's Formula.cpp
+++ Bhaskara's Formula.cpp
@@ -7,26 +7,23 @@ int main()
     double a, b, c, r1, r2;
 
     cin >> a >> b >> c;
-    if (a != 0 && b != 0 && c != 0 && sqrt( (b * b)-( 4 * a * c) ) > 0)
-        {
-        r1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
-        r2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
+    
+    if (a != 0 && b != 0 && c != 0 && ((b * b)-( 4.00 * a * c)  > 0)) {
+        r1 = (-b + sqrt(pow(b, 2) - 4.00*a*c)) / (2*a);
+        r2 = (-b - sqrt(pow(b, 2) - 4.00*a*c)) / (2*a);
         cout << fixed << setprecision(5) << r1 << endl;
         cout << fixed << setprecision(5) << r2 << endl;
-        }
-    else
-        cout << "Impossivel calcular" << endl;
-  
+    }
+        
+        
+    else cout << "Impossivel calcular" << endl;
 
-    /*
-    formula**
+    /*  **FORMULA 
     
     x1 = (-b + sqrt( (bb)-( 4ac) ) )/ 2a
 
     x2 = (-b - sqrt( (bb)-( 4ac) ) )/ 2a
     */
-
-
 
     return 0;
 }
